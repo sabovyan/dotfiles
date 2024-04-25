05bee828ec1738cdd4e7889e4fcc4c076d353c9b
sabovyan <abovyan.sargis90@gmail.com>
2024-04-25 18:05:20 +0400

    add bootstrapping

---
+++ bootstrap.sh
@@ -0,0 +1,92 @@
+!# /bin/bash
+
+# bootstap should install all the necessary software for linux
+function bootstrap() {
+  echo "Bootstrapping..."
+
+  # install nerd fonts
+  echo "Install getnf" # https://github.com/ronniedroid/getnf.git
+
+  # INFO:  ------ terminal setup ------
+
+  # - setup fonts with JetBrainsMono
+  echo "Setting up fonts..."
+
+  # Install zsh
+  echo "Installing zsh..."
+
+  # install oh-my-zsh
+  echo "Installing oh-my-zsh..."
+
+  # install starship
+  echo "Installing starship..."
+#  mkdir -p ~/.config
+
+
+
+  # install exa
+  echo "Installing exa..."
+
+  # install zoxide
+  echo "Installing zoxide..."
+    # setup zooxide history ???
+
+  # install bat
+  echo "Installing bat..."
+
+
+  # install fzf make sure to install it form github
+  echo "Installing fzf..."
+  # setup fzf 
+
+  # install ripgrep
+  echo "Installing ripgrep..."
+
+  # install fd
+  echo "Installing fd..."
+
+
+  # INFO: --- coding setup ---
+
+  # install docker
+  echo "Installing docker..."
+
+  # install gh cli
+  echo "Installing gh..."
+  
+  # install tmux
+  echo "Installing tmux..."
+
+      # setup tmux configurations
+      echo "copying tmux configurations..."
+
+  # install neovim
+  echo "Installing neovim..."
+      # install lazygit
+      echo "Installing lazygit..."
+
+      # install lazydocker
+      echo "Installing lazydocker..."
+
+      # setup neovim configurations
+      echo "copying neovim configurations..."
+
+  # install node
+  echo "Installing node..."
+
+      # install pnpm
+      echo "Installing pnpm..."
+
+
+  # INFO: --- setup personal files ---
+  
+  # setup notes
+
+  # setup projects
+  
+  # setup playground
+  
+  # setup learn
+
+
+}
