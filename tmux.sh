1423dccc8ab76894856e3cd7ed38ea56386ef11c
sabovyan <abovyan.sargis90@gmail.com>
2024-04-25 18:05:10 +0400

    change tmux sart

--- tmux.sh
+++ tmux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+mode=$1
 
-function tm_start_work() {
-
-    local WORK_DIR="/home/sabovyan/projects/uc/diamond/data_volumes/microservice_catalog/ucraft-react/"
+function tm_work {
+    local WORK_DIR=/home/sabovyan/projects/uc/diamond/data_volumes/microservice_catalog/ucraft-react/
     
     if [ ! -d "$WORK_DIR" ]; then
       echo "$WORK_DIR does not exist."
@@ -16,7 +16,6 @@ function tm_start_work() {
     # create three new sessions
     tmux new-session -d -s "editor"
     tmux new-session -d -s "server"
-    tmux new-session -d -s "local"
 
     # open vim in the editor session
     tmux send-keys -t editor:1 'vim' C-m
@@ -28,13 +27,34 @@ function tm_start_work() {
     tmux send-keys -t server:1.0 'cd packages/ds' C-m
     tmux send-keys -t server:1.0 'yarn start' C-m
 
-    # cd into home/sabobyan/.config/nvim in the local session
-    tmux send-keys -t local:1 'cd /home/sabovyan/.config/nvim' C-m
-
-
     tmux attach-session -t editor
+}
 
+function tm_learn {
+    cd /home/sabovyan/learn || { echo "Failed to change directory to /home/sabovyan/learn"; exit 1; }
+
+    # learn should include just one session with three windows
+    tmux new-session -d -s "learn"
+    tmux new-window -t learn:1 -n "learn:notes"
+    tmux send-keys -t learn:1 "nvim" C-m
+
+
+    # tnux send-keys -t learn:1 'nvim' C-m
+
+    tmux attach-session -t learn
 }
 
 
-tm_start_work
+function tm_start {
+
+    if [ "$mode" = "work" ]; then
+      tm_work
+    elif [ "$mode" = "learn" ]; then
+      tm_learn
+    else
+      echo "Invalid mode: $mode"
+      exit 1
+    fi
+}
+
+tm_start $mode
